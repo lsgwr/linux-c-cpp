63dd14622e587d9e7ead2ee65f1761af67fee268
梁山广 <1648266192@qq.com>
2019-03-18 15:46:13 +0800

    Create main.c

---
+++ C深度解析/第05章_Linux和Windows上制作静态库和动态库/libmake_demo_caculate/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>		
+#include "./static_lib/caculate.h"  //包含静态库的头文件
+
+int main(void)
+{
+    double a = 10.5;
+    double b = 20.6;
+    double ret = 0;
+
+    ret = add(a, b); //加
+    printf("add: ret = %f\n", ret);
+
+    ret = sub(a, b); //减
+    printf("sub: ret = %f\n", ret);
+
+    ret = mul(a, b); //乘
+    printf("mul: ret = %f\n", ret);
+    ret = div(a, b);
+    printf("div: ret = %f\n", ret);
+
+    ret = power(a);  //除
+    printf("power: ret = %f\n", ret);
+
+    return 0;
+}
