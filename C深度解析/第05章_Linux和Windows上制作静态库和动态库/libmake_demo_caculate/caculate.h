d9b8b8ec85ef08c4c8d3943ebec633422c06e59e
梁山广 <1648266192@qq.com>
2019-03-18 15:21:48 +0800

    Create caculate.h

---
+++ C深度解析/第05章_Linux和Windows上制作静态库和动态库/libmake_demo_caculate/caculate.h
@@ -0,0 +1,10 @@
+#ifndef H_CACULATE_H
+#define H_CACULATE_H
+
+extern double add(double arg1, double arg2);
+extern double sub(double arg1, double arg2);
+extern double mul(double arg1, double arg2);
+extern double div(double arg1, double arg2);
+extern double power(double arg);
+
+#endif
