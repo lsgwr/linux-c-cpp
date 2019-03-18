56854feebb0bd5fb0fc71e960faf77b3754645e5
梁山广 <1648266192@qq.com>
2019-03-18 15:22:39 +0800

    Create add_sub.c

---
+++ C深度解析/第05章_Linux和Windows上制作静态库和动态库/libmake_demo_caculate/add_sub.c
@@ -0,0 +1,19 @@
+#include "caculate.h"
+
+double add(double arg1, double arg2)
+{
+	double sum = 0;
+
+	sum = arg1 + arg2;
+
+	return sum;
+}
+
+double sub(double arg1, double arg2)
+{
+	double sum = 0;
+
+	sum = arg1 - arg2;
+
+	return sum;
+}
