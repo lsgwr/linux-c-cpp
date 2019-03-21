6866e23d46341fe0be6987b0ca1f1c01923a1128
梁山广 <1648266192@qq.com>
2019-03-21 15:02:30 +0800

    Create public.h

---
+++ C深度解析/第07章_结构体-联合体-枚举/结构体加函数指针模拟面向对象的代码示例/public.h
@@ -0,0 +1,19 @@
+#ifndef H_PUBLIC_H 
+#define H_PUBLIC_H
+
+
+/* 用于想公共代码注册操作信息的结构体 */
+struct Register
+{
+    void *buf;      //数组第一个元素的地址
+    int n;          //数组元素个数
+
+    void (*print)(); //打印函数的指针
+    void (*alter)(); //修改函数的指针
+};
+
+extern void registerFun(struct Register *reginfo);
+extern void printFun(void);
+extern void alterFun(void);
+
+#endif
