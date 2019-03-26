a4e1d2034e8e7a296c11c6f3d978a67694aa726a
梁山广 <1648266192@qq.com>
2019-03-26 11:01:35 +0800

    Create dList_base_op.h

---
+++ C深度解析/第10章_多线程-网络编程-数据库-链表/05_双链表案例的完整代码/dList_base_op.h
@@ -0,0 +1,29 @@
+#ifndef H_SLIST_BASE_OP_H
+#define H_SLIST_BASE_OP_H
+#include <stdlib.h> //exit函数需要这个头文件
+
+#define print_error(str) \
+do{\
+    fprintf(stderr, "File %s, Line %d, Function %s error\n", __FILE__, __LINE__, str);\
+    perror("error Reason");\
+    exit(-1);\
+}while(0);
+
+typedef struct SignalNode  //单链表结构
+{
+    void *data;
+    struct SignalNode *prev; //指向上一个元素
+    struct SignalNode *next; //指向下一个元素
+}DNode;
+
+
+extern DNode *Dou_CreateNode(void *data, int dataSize);
+extern void Dou_insertNode(DNode *prev, DNode *next, DNode *nodep);
+extern void Dou_InsertNodeAfter(DNode *p, DNode *nodep);
+extern void Dou_InsertNodeBefore(DNode *p, DNode *nodep);
+extern void Dou_Delete(DNode *prev, DNode *next);
+extern void Dou_DeleteNode(DNode *prevNodep);
+extern void Dou_HeadInertNode(DNode *hp, DNode *node);
+extern void Dou_TailInertNode(DNode *hp, DNode *node);
+
+#endif
