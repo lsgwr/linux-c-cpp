59894e4265390095f691e446b001a867e1b27c74
梁山广 <1648266192@qq.com>
2019-04-07 19:05:36 +0800

    Update sem.h

--- Linux系统及网络编程视频/第07章_本机进程间通信IPC/my_ipc/sync1/sem.h
+++ Linux系统及网络编程视频/第07章_本机进程间通信IPC/my_ipc/sync1/sem.h
@@ -39,7 +39,7 @@ union semun { // 用于setctl的cmd参数赋值的结构体
 /* 4.函数声明 */
 extern int create_or_get_sem(int nsems);
 extern void init_sem(int semid, int semnum, int val);
-extern void del_sem(int semid, int nsems);
+extern void del_sem(int semid);
 extern void p_sem(int semid, int semnum_arr[], int nsops);
 extern void v_sem(int semid, int semnum_arr[], int nsops);
 
