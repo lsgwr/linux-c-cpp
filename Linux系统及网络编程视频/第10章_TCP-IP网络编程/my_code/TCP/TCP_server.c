336d67ce4f6c2431fc598117d33569f0327f8037
liang shan guang <1648266192@qq.com>
2019-04-13 23:33:30 +0800

    bind函数快讲完了

--- Linux系统及网络编程视频/第10章_TCP-IP网络编程/my_code/TCP/TCP_server.c
+++ Linux系统及网络编程视频/第10章_TCP-IP网络编程/my_code/TCP/TCP_server.c
@@ -15,15 +15,16 @@ do{\
 int main(int argc, char const *argv[])
 {
     int ret = -1;
-    int skfd = -1;
+
     /* 第1步：创建使用TCP协议通信的套接字文件*/
+    int skfd = -1;
     skfd = socket(AF_INET, SOCK_STREAM, 0);
     if(skfd == -1) print_error("socket fail");
     
-    struct sockaddr_in addr; // 必须引入arpa/inet.h
-    addr.sin_family = AF_INET; //使用是IPV4 TCP/IP协议族的ip地址(32位)
-    addr.sin_port   = htons(5006);//指定端口
-    addr.sin_addr.s_addr = inet_addr("192.168.1.105");//指定ip地址
-    ret = bind(skfd, (struct sockaddr*)&addr, sizeof(addr));
+    /* 第2步：调用bind绑定服务器的套接字、ip、端口 */
+    struct sockaddr_in server_addr; 
+    server_addr.sin_family = AF_INET; // 使用是IPV4 TCP/IP协议族的ip地址(32位)
+    server_addr.sin_port = htons(5006); // 指定端口
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     return 0;
 }
