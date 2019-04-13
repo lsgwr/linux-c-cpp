18ab15e8d9bbf733571fe97857e699768b314499
liang shan guang <1648266192@qq.com>
2019-04-13 18:26:12 +0800

    socket函数的domain参数

---
+++ Linux系统及网络编程视频/第10章_TCP-IP网络编程/my_code/TCP/TCP_server.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+int main(int argc, char const *argv[])
+{
+    int skfd;
+    return 0;
+}
