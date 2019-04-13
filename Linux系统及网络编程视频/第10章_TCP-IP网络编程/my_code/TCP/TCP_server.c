656186946351fa8a52cb39fcc93f61655d065e2e
liang shan guang <1648266192@qq.com>
2019-04-13 19:24:30 +0800

    socket函数彻底学完了

--- Linux系统及网络编程视频/第10章_TCP-IP网络编程/my_code/TCP/TCP_server.c
+++ Linux系统及网络编程视频/第10章_TCP-IP网络编程/my_code/TCP/TCP_server.c
@@ -4,9 +4,18 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 
+#define print_error(str) \
+do{\
+    fprintf(stderr, "File %s, Line %d, Function %s error\n",__FILE__, __LINE__, __func__);\
+    perror(str);\
+    exit(-1);\
+}while(0);
+
 int main(int argc, char const *argv[])
 {
-    int skfd;
+    int skfd = -1;
+    /* 第1步：创建使用TCP协议通信的套接字文件*/
     skfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(skfd == -1) print_error("socket fail");
     return 0;
 }
