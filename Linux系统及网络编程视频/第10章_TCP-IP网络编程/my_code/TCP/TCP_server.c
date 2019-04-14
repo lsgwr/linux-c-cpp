25c0dff2482014faf9fa8b58e4982d860ede4b41
liang shan guang <1648266192@qq.com>
2019-04-14 20:10:40 +0800

    发送数据send完成啦

--- Linux系统及网络编程视频/第10章_TCP-IP网络编程/my_code/TCP/TCP_server.c
+++ Linux系统及网络编程视频/第10章_TCP-IP网络编程/my_code/TCP/TCP_server.c
@@ -13,6 +13,12 @@ do{\
     exit(-1);\
 }while(0);
 
+/* tcp通信时发送的数据(send函数使用)，这里以一个学生信息为例 */
+typedef struct tcpdata{
+    unsigned int stu_num; // 学号,无符号整型
+    char stu_name[30]; // 学生姓名
+}student;
+
 int main(int argc, char const *argv[])
 {
     int ret = -1;
@@ -35,11 +41,27 @@ int main(int argc, char const *argv[])
     if(ret == -1) print_error("listen fail");
 
     /* 第4步：调用accept 网络API，被动监听客户发起三次握手的连接请求，三次握手成功，即建立连接成功. 被动监听客户的连接也被称为监听客户上线的过程*/
-    int cfd = -1; // 客户端用于和服务器端通信的fd
+    int cfd = -1; // 服务器端用于和指定客户端通信的指定fd
     struct sockaddr_in clnaddr = {}; // clnaddr : client address,用于存储客户端信息(ip和端口)的结构体
     unsigned int clnaddr_size = sizeof(clnaddr);
     cfd = accept(skfd, (struct sockaddr *)&clnaddr, &clnaddr_size); // 接收客户端相关的信息，成功返回客户端用于和服务器端通信的fd
     if(cfd == -1) print_error("accept fail");
     printf("cln_port= %d, cln_addr=%s\n", ntohs(clnaddr.sin_port), inet_ntoa(clnaddr.sin_addr)); // 打印客户端端口和IP，一定要先进行端序转换
+
+    /* 第5步：服务器调用read(recv)和write(send)，收发数据，实现与客户的通信 */
+    student stu_data = {0};
+    unsigned int tmp_num; // 定义临时变量用于字节序转换
+    while(1){
+        // 获取学生学号,但是需要从主机端序转换为网络端序
+        printf("Please input student number:\n"); 
+        scanf("%d", &tmp_num);
+        stu_data.stu_num = htonl(tmp_num);
+        // 获取学生姓名，不需要进行端序转换,因为字符数组以一个字节为单位进行存储
+        printf("Please input student name:\n"); // 获取学生姓名
+        scanf("%s", stu_data.stu_name);
+        // 根据cfd向指定的客户端发送数据
+        ret = send(cfd, (void *)&stu_data, sizeof(stu_data), 0);
+        if(ret == -1) print_error("send fail");
+    }
     return 0;
 }
