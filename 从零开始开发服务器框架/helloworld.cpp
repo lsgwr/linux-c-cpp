4fee5283de38010b7149a4f2b3f17ff624eca074
19920625lsg <1648266192@qq.com>
2019-06-19 00:16:00 +0000

    add helloworld

---
+++ 从零开始开发服务器框架/helloworld.cpp
@@ -0,0 +1,8 @@
+#include <iostream>         //类似于头文件来理解
+int main()                  //主函数
+{
+    using namespace std;    //记得写就行
+    cout << "HelloWorld\n"; //打印“helloworld"
+    //cout << endl;         //这句是单独打一个回车的意思，因为题目不要求，我们就不打了
+    return 0;
+}
