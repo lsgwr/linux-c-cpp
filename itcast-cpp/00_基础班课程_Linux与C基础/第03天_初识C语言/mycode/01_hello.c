0543c19ccf4995ca132f6931ca7a75cf0290deec
19920625lsg <1648266192@qq.com>
2020-01-09 00:29:44 +0800

    system和C基础讲义

---
+++ itcast-cpp/00_基础班课程_Linux与C基础/第03天_初识C语言/mycode/01_hello.c
@@ -0,0 +1,12 @@
+/***********************************************************
+ * @Description : 
+ * @author      : 梁山广(Liang Shan Guang)
+ * @date        : 2020/1/8 23:12
+ * @email       : liangshanguang2@gmail.com
+ ***********************************************************/
+#include <stdio.h>
+
+int main() {
+    printf("hello");
+    return 0;
+}
