2f77680275d88a27e2c3a36c92acd321b29827a2
19920625lsg <1648266192@qq.com>
2020-01-10 07:47:11 +0800

    基础讲义

---
+++ itcast-cpp/00_基础班课程_Linux与C基础/第04天_数据类型与运算法/mycode/buma.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    int a = -15;
+    printf("%x\n", a);
+    return 0;
+}
---
+++ itcast-cpp/00_基础班课程_Linux与C基础/第04天_数据类型与运算法/mycode/format.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a = 123; // 10进制数
+    int b = 0567; // 8进制数
+    int c = 0xabc; // 十六进制数
+
+    printf("a=%d\n", a);
+
+    return 0;
+}
