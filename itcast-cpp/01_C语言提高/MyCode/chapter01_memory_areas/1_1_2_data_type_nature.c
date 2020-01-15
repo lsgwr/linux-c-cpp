2710cfa4de210d7dd39366864ff42cc181b9d81f
19920625lsg <1648266192@qq.com>
2020-01-16 07:52:19 +0800

    第1章 开始啦~~内存四区~~

---
+++ itcast-cpp/01_C语言提高/MyCode/chapter01_memory_areas/1_1_2_data_type_nature.c
@@ -0,0 +1,17 @@
+/***********************************************************
+ * @Description : 数据类型的本质
+ * @author      : 梁山广(Liang Shan Guang)
+ * @date        : 2020/1/16 7:40
+ * @email       : liangshanguang2@gmail.com
+ ***********************************************************/
+#include <stdio.h>
+
+int main(void) {
+    int a = 10; // 告诉编译器，分配四个字节的内存
+    int b[10]; // 告诉编译器，分配4*10个字节的内存
+    printf("b:%p\n", b); // b代表数组首元素的地址
+    printf("b+1:%p\n", b+1);
+    printf("&b:%p\n", &b); // &b代表整体数组的地址
+    printf("&b+1:%p\n", &b + 1);
+}
+
