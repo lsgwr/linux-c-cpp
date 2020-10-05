666519a7a7940653bd2994a91043ac82f89b2476
liang shan guang <1648266192@qq.com>
2020-10-05 11:18:00 +0800

    引用的使用规范

--- 01_基础/慕课网_重构你的CPP知识体系/main.cpp
+++ 01_基础/慕课网_重构你的CPP知识体系/main.cpp
@@ -1,74 +1,40 @@
-#include <string>
 #include <iostream>
-#include <memory>
+#include <assert.h>
 
 using namespace std;
 
-struct B;
-
-struct A {
-    shared_ptr<B> pb;
-
-    ~A() {
-        cout << "~A()" << endl;
-    }
-};
-
-struct B {
-    shared_ptr<A> pa; // A和B都是shared_ptr，互相引用造成了循环引用，导致A和B的对象都无法释放，从而产生了内存泄露
-
-    ~B() {
-        cout << "~B()" << endl;
-    }
-};
-
-// pa 和 pb 存在着循环引用，根据 shared_ptr 引用计数的原理，pa 和 pb 都无法被正常的释放。
-// weak_ptr 是为了解决 shared_ptr 双向引用的问题。
-struct BW;
-
-struct AW {
-    shared_ptr<BW> pb;
-
-    ~AW() {
-        cout << "~AW()" << endl;
-    }
-};
-
-struct BW {
-    weak_ptr<AW> pa; // 和B相比，这里把shared_ptr改成了weak_ptr，这是防止循环引用的关键
-
-    ~BW() {
-        cout << "~BW()" << endl;
-    }
-};
-
-void Test() {
-    cout << "Test shared_ptr and shared_ptr:  " << endl;
-    shared_ptr<A> tA(new A());                                             // 1
-    shared_ptr<B> tB(new B());                                             // 1
-    cout << tA.use_count() << endl;
-    cout << tB.use_count() << endl;
-    tA->pb = tB;
-    tB->pa = tA;
-    cout << tA.use_count() << endl;                                        // 2
-    cout << tB.use_count() << endl;                                        // 2
+// 编写一个函数，输入两个int型变量a,b
+// 实现在函数内部将a,b的值进行交换。
+void swap(int &a, int &b) {
+    int tmp = a;
+    a = b;
+    b = tmp;
 }
 
-void Test2() {
-    cout << "Test weak_ptr and shared_ptr:  " << endl;
-    shared_ptr<AW> tA(new AW());
-    shared_ptr<BW> tB(new BW());
-    cout << tA.use_count() << endl;                                        // 1
-    cout << tB.use_count() << endl;                                        // 1
-    tA->pb = tB;
-    tB->pa = tA;
-    cout << tA.use_count() << endl;                                        // 1
-    cout << tB.use_count() << endl;                                        // 2
+void swap2(int *a, int *b) {
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
 }
 
 int main() {
-    Test();
-    Test2();
+    int x = 1, x2 = 3;
+    int &rx = x;
+    rx = 2;
+    cout << x << endl;     // 2
+    cout << rx << endl;    // 2
+    rx = x2;
+    cout << x << endl;     // 3
+    cout << rx << endl;    // 3
+
+    // 交换变量的测试
+    int a = 3, b = 4;
+    swap(a, b);   // 引用交换
+    assert(a == 4 && b == 3);
+
+    a = 3, b = 4;
+    swap2(&a, &b);
+    assert(a == 4 && b == 3);
 
 
     return 0;
