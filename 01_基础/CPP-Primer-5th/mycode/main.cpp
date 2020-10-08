625e665b77ae2f5139f9fd74241a0522fc191e0d
19920625lsg <1648266192@qq.com>
2020-10-08 23:36:49 +0800

    添加pdf

--- 01_基础/CPP-Primer-5th/mycode/main.cpp
+++ 01_基础/CPP-Primer-5th/mycode/main.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
-#include "Sales_item.h"
+#include <memory>
 
 using namespace std;
+
+struct dest {
+};
+struct conn {
+};;
+
+conn connect(dest *pd) {
+    cout << "打开连接" << endl;
+    return conn();
+}
+
+void disconnect(conn c) {
+    cout << "关闭连接" << endl;
+}
+
+// 未使用shared_ptr的版本
+void f(dest &d) {
+    cout << "直接管理连接" << endl;
+    conn c = connect(&d);
+    // Todo:忘记调用disconnect关闭连接
+    cout << endl;
+}
+
+void end_connection(conn *p) {
+    disconnect(*p);
+}
+
+// 使用shared_ptr的版本
+void f1(dest &d) {
+
+}
 
 int main() {
 
