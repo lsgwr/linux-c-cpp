85628825cab4041dbb0368cbe7d7304eea88f833
19920625lsg <1648266192@qq.com>
2020-10-08 11:22:36 +0800

    慕课网的仿函数

--- 01_基础/慕课网_重构你的CPP知识体系/main.cpp
+++ 01_基础/慕课网_重构你的CPP知识体系/main.cpp
@@ -1,77 +1,78 @@
-#include <iostream> // std::cout
-#include <vector> // std::vector
-#include <algorithm> // std::sort
-#include <functional> // std::binary_function
-#include <iterator>
-#include <numeric>
-#include <string>
-#include <unordered_map>
+#include <algorithm>
+#include <iostream>
 
-//函数指针类别：返回S 无输入  通过指针调用
-template<class S, class T>
-class mem_fun_t : public std::unary_function<T *, S> {
-public:
-    explicit mem_fun_t(S (T::*pf)()) : f(pf) {}//初始化
-    S operator()(T *p) const { return (p->*f)(); }//调用，p里面对应的函数
-private:
-    S (T::*f)();//这是一个变量，这个函数指针变量
-};
+using namespace std;
 
-//辅助函数，直接通过模板萃取相应的型别，然后声明相应的对象
-template<class S, class T>
-inline mem_fun_t<S, T> mem_fun(S (T::*f)()) {
-    return mem_fun_t<S, T>(f);//返回仿函数临时对象，真的很牛逼哦抽象出来了
+bool MySort(int a, int b) {
+    return a < b;
 }
 
-// 小例子
-// size_type length() const { return _M_string_length; }
-//
-// mem_fun(&string::length);//传入函数指针，::优先级大于&
-//s 是 size_type  T是string  f是length，通过模板萃取出这些型别，即可产生相应的对象。
+void Display(int a) {
+    cout << a << " ";
+}
 
-//有一个参数，通过指针调用
-template<class S, class T, class A>
-class mem_fun1_t : public std::binary_function<T *, A, S> {
-public:
-    explicit mem_fun1_t(S (T::*pf)(A)) : f(pf) {}
+template<class T>
+inline bool MySortT(T const &a, T const &b) {
+    return a < b;
+}
 
-    S operator()(T *p, A x) const { return (p->*f)(x); }
+template<class T>
+inline void DisplayT(T const &a) {
+    cout << a << " ";
+}
 
-private:
-    S (T::*f)(A);
+struct SortF {
+    bool operator()(int a, int b) {
+        return a < b;
+    }
 };
 
-template<class S, class T, class A>
-class const_mem_fun1_t : public std::binary_function<const T *, A, S> {
-public:
-    explicit const_mem_fun1_t(S (T::*pf)(A) const) : f(pf) {}
-
-    S operator()(const T *p, A x) const { return (p->*f)(x); }
-
-private:
-    S (T::*f)(A) const;
+struct DisplayF {
+    void operator()(int a) {
+        cout << a << " ";
+    }
 };
+
+// C++仿函数模板
+template<class T>
+struct SortTF {
+    inline bool operator()(T const &a, T const &b) const {
+        return a < b;
+    }
+};
+
+template<class T>
+struct DisplayTF {
+    inline void operator()(T const &a) const {
+        cout << a << " ";
+    }
+};
+
 
 int main() {
-    std::vector<std::string *> numbers;
+    // C++方式
+    int arr[] = {4, 3, 2, 1, 7};
+    sort(arr, arr + 5, MySort);
+    for_each(arr, arr + 5, Display); // 1 2 3 4 7
+    cout << endl;
 
-    // populate vector of pointers:
-    numbers.push_back(new std::string("one"));
-    numbers.push_back(new std::string("two"));
-    numbers.push_back(new std::string("three"));
-    numbers.push_back(new std::string("four"));
-    numbers.push_back(new std::string("five"));
+    // C++泛型
+    int arr2[] = {4, 3, 2, 1, 7};
+    sort(arr2, arr2 + 5, MySortT<int>);
+    for_each(arr2, arr2 + 5, DisplayT<int>); // 1 2 3 4 7
+    cout << endl;
 
-    std::vector<int> lengths(numbers.size());//预先分配内存空间
+    // C++仿函数
+    int arr3[] = {4, 3, 2, 1, 7};
+    sort(arr3, arr3 + 5, SortTF<int>());
+    for_each(arr3, arr3 + 5, DisplayTF<int>()); // 1 2 3 4 7
+    cout << endl;
 
-    transform(numbers.begin(), numbers.end(), lengths.begin(), mem_fun(&std::string::length));
-
-    for (int i = 0; i < 5; i++) {
-        std::cout << *numbers[i] << " has " << lengths[i] << " letters.\n";
-    }
-
-    // deallocate strings:
-    for (auto &number : numbers) delete number;
+    // C++仿函数模板
+    int arr4[] = {4, 3, 2, 1, 7};
+    sort(arr4, arr4 + 5, SortF());
+    for_each(arr4, arr4 + 5, DisplayF()); // 1 2 3 4 7
+    cout << endl;
 
     return 0;
 }
--- 01_基础/慕课网_重构你的CPP知识体系/第09章_CPP进阶编程/README.md
+++ 01_基础/慕课网_重构你的CPP知识体系/第09章_CPP进阶编程/README.md
@@ -912,3 +912,89 @@ three has 5 letters.
 four has 4 letters.
 five has 4 letters.
 ```
+
+### 4.慕课网课程的总结
++ 仿函数一般不会单独使用，主要是为了搭配STL算法使用
++ 函数指针不能满足STL对抽象性的要求，不能满足软件积木的要求，无法和其他STL组件搭配
++ 本质就是类重载了一个`operator()`，创建一个行为类似函数的对象
+
+```cpp
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+bool MySort(int a, int b) {
+    return a < b;
+}
+
+void Display(int a) {
+    cout << a << " ";
+}
+
+template<class T>
+inline bool MySortT(T const &a, T const &b) {
+    return a < b;
+}
+
+template<class T>
+inline void DisplayT(T const &a) {
+    cout << a << " ";
+}
+
+struct SortF {
+    bool operator()(int a, int b) {
+        return a < b;
+    }
+};
+
+struct DisplayF {
+    void operator()(int a) {
+        cout << a << " ";
+    }
+};
+
+// C++仿函数模板
+template<class T>
+struct SortTF {
+    inline bool operator()(T const &a, T const &b) const {
+        return a < b;
+    }
+};
+
+template<class T>
+struct DisplayTF {
+    inline void operator()(T const &a) const {
+        cout << a << " ";
+    }
+};
+
+
+int main() {
+    // C++方式
+    int arr[] = {4, 3, 2, 1, 7};
+    sort(arr, arr + 5, MySort);
+    for_each(arr, arr + 5, Display); // 1 2 3 4 7 
+    cout << endl;
+
+    // C++泛型
+    int arr2[] = {4, 3, 2, 1, 7};
+    sort(arr2, arr2 + 5, MySortT<int>);
+    for_each(arr2, arr2 + 5, DisplayT<int>); // 1 2 3 4 7 
+    cout << endl;
+
+    // C++仿函数
+    int arr3[] = {4, 3, 2, 1, 7};
+    sort(arr3, arr3 + 5, SortTF<int>());
+    for_each(arr3, arr3 + 5, DisplayTF<int>()); // 1 2 3 4 7 
+    cout << endl;
+
+    // C++仿函数模板
+    int arr4[] = {4, 3, 2, 1, 7};
+    sort(arr4, arr4 + 5, SortF());
+    for_each(arr4, arr4 + 5, DisplayF()); // 1 2 3 4 7 
+    cout << endl;
+
+    return 0;
+}
+```
