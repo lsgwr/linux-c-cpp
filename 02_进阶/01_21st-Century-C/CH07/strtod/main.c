efd17c7d03c1501ba93fffa1c370244b210dd49c
19920625lsg <1648266192@qq.com>
2022-05-24 20:32:08 +0800

    字符串转数字

--- 02_进阶/01_21st-Century-C/CH07/strtod/main.c
+++ 02_进阶/01_21st-Century-C/CH07/strtod/main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include "stopif.h"
 
 int main() {
-    printf("Hello, World!\n");
+    char *end;
+    char *str = "12XX";
+    double in = strtod(str, &end); // end指向第1个不能解析成数字的字母
+    Stopif(*end, return 2, "I couldn't parse '%s' to a number. i had trouble with '%s'.", str, end);
+    printf("The square of %s is %g\n", str, pow(in, 2));
     return 0;
 }
+/**
+ * I couldn't parse '12XX' to a number. i had trouble with 'XX'.
+ */
